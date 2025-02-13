56753d69c974a5a2ad843722d144e2cf67b0b4a6
Traincape <traincapetech0641@gmail.com>
2025-02-13 11:53:21 +0530

    footer

--- client/src/routes/AllRoutes.jsx
+++ client/src/routes/AllRoutes.jsx
@@ -3,14 +3,21 @@ import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
 import Login from '../pages/Login'
 import Signup from '../pages/Signup'
-
+import About from '../pages/About'
+import Blog from '../pages/Blog'
+import Careers from '../pages/Career'
+import SearchForDoctors from '../pages/SearchForDoctors'
 const AllRoutes = () => {
   return (
-  <Routes>
-    <Route path='/' element={<Home />}/>
-    <Route path='/login' element={<Login />}/>
-    <Route path='/signup' element={<Signup />}/>
-  </Routes>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/About' element={<About />} />
+      <Route path='/Blog' element={<Blog/>}/>
+      <Route path='/Careers' element={<Careers/>}/>
+      <Route path='SearchForDoctors' element={<SearchForDoctors/>}/>
+    </Routes>
   )
 }
 
